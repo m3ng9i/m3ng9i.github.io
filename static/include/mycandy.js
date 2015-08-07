3c65de475ee759449f7689c644233d31dad6d747
mengqi <5b5f7426@gmail.com>
2015-08-07 10:18:21 +0800

    update css and js

---
+++ static/include/mycandy.js
@@ -0,0 +1,56 @@
+$(function() {
+    // set color of asterisk
+    $(".mycandy-brand").hover(function() {
+        $(this).children("sup").removeClass("mycandy-asterisk").addClass("mycandy-asterisk-hover");
+    }, function() {
+        $(this).children("sup").removeClass("mycandy-asterisk-hover").addClass("mycandy-asterisk");
+    });
+
+    // set color of rss logo
+    $("#navbar-links ul[class~='navbar-right'] span[class~='fa-rss-square']").parent().hover(function() {
+        $(this).children("span[class~='fa-rss-square']").removeClass("mycandy-rss").addClass("mycandy-rss-hover");
+    }, function() {
+        $(this).children("span[class~='fa-rss-square']").removeClass("mycandy-rss-hover").addClass("mycandy-rss");
+    });
+
+    // set responsive image
+    $("article .mycandy-article img").addClass("img-responsive");
+
+    // open a new window when click a github project url
+    $("#navbar-links ul.mycandy-github a").each(function() {
+        $(this).click(function() {
+            window.open(this.href);
+            return false;
+        });
+    });
+
+    // set active class on navbar
+    (function() {
+        var navButton = $("#navbar-links > ul > li");
+
+        var setNavActiveState = function() {
+            var selected = null;
+
+            switch(window.location.pathname) {
+                case "/":                   selected = navButton.get(0); break;
+                case "/html/about.html":    selected = navButton.get(1); break;
+            }
+
+            if (selected !== null) {
+                $(selected).addClass("active");
+            }
+        };
+
+        setNavActiveState();
+
+        $(navButton.get(2)).on("show.bs.dropdown", function() {
+            $(navButton.get(0)).removeClass("active");
+            $(navButton.get(1)).removeClass("active");
+        });
+
+        $(navButton.get(2)).on("hide.bs.dropdown", function() {
+            setNavActiveState();
+        });
+    })();
+
+});
